9ffcf36663ec67a42f2e5186054ad205ab34c452
nicolo_canevari <nico.canevari@outlook.it>
2025-02-06 16:20:28 +0100

    [FEAT] function show

--- controllers/controller_posts.js
+++ controllers/controller_posts.js
@@ -1,17 +1,31 @@
 // importo il file data_posts
-const posts = require('../data/data_posts');
+const posts = require('../data/posts');
 
 // index => funzione
 function index(req, res) {
 
-    res.send('Visualizza tutti gli elementi');
+    // Restituisce la lista completa dei post in formato JSON
+    res.send(posts);
 
 }
 
 // show => funzione
 function show(req, res) {
 
-    res.send('Visualizza un elemento');
+    // interpreta la stinga come numero decimale
+    const postId = parseInt(req.params.id, 10);
+    // ricerca dell'ID specificato
+    const post = post.find(post => post.id === postID);
+
+    // se il post non venisse trovato restituisco error 404
+    if (!post) {
+
+        return res.status(404).json({ error: 'Post non trovato' });
+
+    }
+
+    // Se il post esiste, restituisce i dettagli del post in formato JSON
+    res.json(post);
 
 }
 
